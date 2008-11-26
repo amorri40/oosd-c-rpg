530894fbb2cc18fdfd612e44508bb0b43edb6b7d
jw271206 <jw271206@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-11-26 13:01:56 +0000

    screen coding

---
+++ trunk/the folder/splash screen.py
@@ -0,0 +1,13 @@
+import sys, pygame
+class Splashscreen:  
+    def __init__(self):
+        pygame.init()
+        x = 0
+        background = pygame.image.load('mario.jpg')  #imports the image to display
+        size = width, height = 800, 600 # sets screen dimensions
+        screen = pygame.display.set_mode(size) #creates screen
+        screen.blit(background, (0, 0)) # imports background into screen
+        pygame.display.update()
+        pygame.time.wait(3000) #screen displays for 3 seconds
+
+splash = Splashscreen()
