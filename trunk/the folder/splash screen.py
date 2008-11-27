f6ff4f8c18ec0dbaf825414e3eddd0780d2b78ab
amorri40 <amorri40@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-11-27 14:38:22 +0000

    changed image

--- trunk/the folder/splash screen.py
+++ trunk/the folder/splash screen.py
@@ -3,7 +3,7 @@ class Splashscreen:
     def __init__(self):
         pygame.init()
         x = 0
-        background = pygame.image.load('mario.jpg')  #imports the image to display
+        background = pygame.image.load('stein.jpg')  #imports the image to display
         size = width, height = 800, 600 # sets screen dimensions
         screen = pygame.display.set_mode(size) #creates screen
         screen.blit(background, (0, 0)) # imports background into screen
