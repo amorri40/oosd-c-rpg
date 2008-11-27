1758fc9799aff545e88c29e1b118093a7295497e
amorri40 <amorri40@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-11-27 14:06:17 +0000

    updated mainscene

--- trunk/game code/MainScene.py
+++ trunk/game code/MainScene.py
@@ -8,17 +8,32 @@ class MainScene:
     The Mainscene class is where the main game takes place.
     """
     def __init__(self):
+        """
+        Constructor used for initialising the main scene
+        """
         global pl
         pl = Player()
         global screen,screenbackground
         screen = pygame.display.get_surface() 
         screenbackground = pygame.Surface(pygame.display.get_surface().get_size())
         screenbackground.fill((0,0,0))
+        
+        """
+        Setup background
+        """
         global background,view
-        background = pygame.Surface((1000,1000))
-       
+        
+        background = pygame.Surface((1042,1024))
+        
         background = background.convert()
-        background.fill((51, 153, 0))
+        backimg = os.path.join("images","background.png")
+        backimage = pygame.image.load(backimg)
+        #background.fill((51, 153, 0))
+        background.blit(backimage,(0,0))
+        
+        """
+        Setup game objects
+        """
         #Now create all the game objects
         self.createObjects() 
         #draw the objects one by one on to the background image
@@ -27,7 +42,7 @@ class MainScene:
             self.objects[i].draw(background)
             i=i+1
         #create a new view which will be the main play area
-        view = pygame.Surface((1000,1000))
+        view = pygame.Surface((1024,1024))
         
         self.viewx=0
         self.viewy=0
@@ -46,18 +61,24 @@ class MainScene:
         self.objects.append(GameObject(200,200,os.path.join("images","player.png"),pygame.Rect(200,200,18,24)))
     
     def draw(self):
-        view.blit(background, (0, 0))
+        """
+        This will be called for every frame that is drawn, it will
+        draw the scene and update the game.
+        """
+        view.blit(background, (0, 0)) #draw the background image onto the view
         
-        pl.move()
-        self.checkForCollisions()
+        pl.move() #move the player to the new position
+        self.checkForCollisions() #check if the player collides with any game object
         
-        #now draw the player
-        pl.draw(view)
-        self.moveScene()
-        screenbackground.blit(view, (self.viewx, self.viewy))
-        screen.blit(screenbackground,(0,0))
+        pl.draw(view)#draw the player
+        self.moveScene()#move the scene when the player moves
+        screenbackground.blit(view, (self.viewx, self.viewy))#draw the view onto screenbackground
+        screen.blit(screenbackground,(0,0))#draw the main game to the screen
         
     def moveScene(self):
+        """
+        Move the view so that it moves with the player
+        """
         if self.viewx<700:
           self.viewxprevious=self.viewx
           self.viewx = self.viewx+self.viewhspeed
@@ -72,7 +93,7 @@ class MainScene:
       
     def event(self,events): 
         """
-        Event method
+        Handle all input events here
         """
             
         for event in events:  
@@ -112,6 +133,5 @@ class MainScene:
                 pl.x=pl.xprevious
                 pl.y=pl.yprevious
               
-            
             i=i+1
         
