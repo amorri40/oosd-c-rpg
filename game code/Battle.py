64ab347feccff41d9ad4b8f603706823e222c2e7
JordanHolmes <JordanHolmes@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-11-28 16:39:42 +0000

    blah

--- game code/Battle.py
+++ game code/Battle.py
@@ -5,8 +5,8 @@ class Battle:
     #Create two characters to battle
     def __init__(self):
 
-        self.player1 = Character(100, 20, 20)
-        self.enemy = Character(80, 10, 10)
+        self.player1 = Character('Jordan', 100, 20, 20)
+        self.enemy = Character('Enemy', 80, 10, 10)
 
 
     #A method to attack the Enemy
