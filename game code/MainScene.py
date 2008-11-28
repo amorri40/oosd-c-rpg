48e2b83fc986179bbdc0d762491884d9074a3a05
amorri40@gmail.com <amorri40@gmail.com@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-11-28 10:13:20 +0000

    Updated background image

--- game code/MainScene.py
+++ game code/MainScene.py
@@ -27,7 +27,7 @@ class MainScene(Scene):
         background = pygame.Surface((1042,1024))
         
         background = background.convert()
-        backimg = os.path.join("images","background.png")
+        backimg = os.path.join("images","background2.png")
         backimage = pygame.image.load(backimg)
         #background.fill((51, 153, 0))
         background.blit(backimage,(0,0))
