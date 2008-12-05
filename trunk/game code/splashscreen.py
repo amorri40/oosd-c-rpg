bf9e1ee0838ef10689e5674340fbe314a6935abe
jw271206 <jw271206@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-12-05 10:56:47 +0000

    can now make the beer disappear...but the game code still loads it a second time :@

--- trunk/game code/splashscreen.py
+++ trunk/game code/splashscreen.py
@@ -3,11 +3,14 @@ from pygame.locals import *
 class Splashscreen: 
     def __init__(self):
         pygame.init()
-        x = 0
         background = pygame.image.load(os.path.join("images", "stein.jpg"))  #imports the image to display
         size = width, height = 300, 300 # sets screen dimensions
         screen = pygame.display.set_mode(size) #creates screen
+        pygame.display.set_caption('RPG game coursework') 
         screen.blit(background, (0, 0)) # imports background into screen
         pygame.display.update()
-        pygame.time.wait(3000) #screen displays for 3 seconds
-splash = Splashscreen() 
+        pygame.time.wait(3000)#screen displays for 3 seconds
+        screen.fill(0)
+        pygame.display.update()
+        pygame.time.wait(500)
+splash = Splashscreen()
