6ac424733452623fa957dddbf42f8cbaee3b6de5
jw271206 <jw271206@0688d374-b7c9-11dd-8584-61fb05dc49a7>
2008-12-05 11:06:57 +0000

    i got that problem with the image solved finally.

--- trunk/game code/splashscreen.py
+++ trunk/game code/splashscreen.py
@@ -6,11 +6,7 @@ class Splashscreen:
         background = pygame.image.load(os.path.join("images", "stein.jpg"))  #imports the image to display
         size = width, height = 300, 300 # sets screen dimensions
         screen = pygame.display.set_mode(size) #creates screen
-        pygame.display.set_caption('RPG game coursework') 
+        pygame.display.set_caption('RPG game coursework') #gives the program a title
         screen.blit(background, (0, 0)) # imports background into screen
         pygame.display.update()
         pygame.time.wait(3000)#screen displays for 3 seconds
-        screen.fill(0)
-        pygame.display.update()
-        pygame.time.wait(500)
-splash = Splashscreen()
